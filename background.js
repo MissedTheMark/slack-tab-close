32f573c10019fc13b489df0c762d3169bfb63b6f
Mark Coafield <46470837+MissedTheMark@users.noreply.github.com>
2021-09-06 17:08:54 +1000

    Added delay

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 // check for a Slack 'Redirecting… | Slack' title with a xyz.slack.com url, wait 5 seconds and then close the tab
 
-var regex = /^((http[s]?):\/)?\/?((\/\w+)*)(.*)(\.slack\.com)(.*)/;
-var title = "Redirecting… | Slack";
+const regex = /^((http[s]?):\/)?\/?((\/\w+)*)(.*)(\.slack\.com)(.*)/;
+const title = "Redirecting… | Slack";
+const delay = 5000;
 
 function closeSlackTab(tabId, changeInfo, tab) {
     if (changeInfo.url) {
         if (regex.test(changeInfo.url) && changeInfo.title === title) {
-            browser.tabs.remove(tabId);
+            setTimeout(() => browser.tabs.remove(tabId), delay);
         }
     }
 }
